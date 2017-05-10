e84167bd060bd418e902a07fd0f11986931fed7c
App Academy Student <student@appacademy.io>
2017-05-09 21:54:56 -0700

    Add homework

---
+++ components/giphys_index_item.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class GiphysIndexItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <li></li>
+    );
+  }
+}
+
+export default GiphysIndexItem;
---
+++ components/giphys_search.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <h1>hahahahah</h1>
+
+    );
+  }
+}
+export default GiphysSearch;
